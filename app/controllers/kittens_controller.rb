313713f7847decc8f4ea8c81dc3c92da04184f22
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2026-01-17 22:24:21 +0530

    rails g controller KittensController

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,22 @@
+class KittensController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  def edit
+  end
+end
