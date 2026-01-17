0c22c510a6d26b9cdd77d18a886ab32d9d4f44dd
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2026-01-17 22:29:12 +0530

    feat(routes): add resources route for kittens

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get "kittens/new"
-  get "kittens/create"
-  get "kittens/index"
-  get "kittens/show"
-  get "kittens/destroy"
-  get "kittens/update"
-  get "kittens/edit"
+  resources :kittens
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
